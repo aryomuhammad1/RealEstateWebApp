923c6fe8bbc0f9a6ef86b8e3533ea4d333ffba41
aryo muhammad <aryomuhammadputra@gmail.com>
2023-05-01 10:58:45 +0700

    adding plaiceholder to config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const { withPlaiceholder } = require("@plaiceholder/next");
+const nextConfig = withPlaiceholder({
   reactStrictMode: true,
   images: {
     domains: ["bayut-production.s3.eu-central-1.amazonaws.com"],
   },
-};
+});
 
 module.exports = nextConfig;
